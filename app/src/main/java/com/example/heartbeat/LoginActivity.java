335060a83092bae0ebb16e36ee2f44740fe041d8
kim-ye-won <nerino5885@gmail.com>
2021-12-21 02:23:54 +0900

    로딩화면 및 로그인 화면 구현 및 회원가입 화면 이동

---
+++ app/src/main/java/com/example/heartbeat/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.heartbeat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button SignUp = (Button) findViewById(R.id.sign_up_btn);
+        SignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
