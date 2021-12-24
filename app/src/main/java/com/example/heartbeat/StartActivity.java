b76fb95ae911146ba832c826c2cea042bf576efe
JeongMin Choi <tlsdnjfwjdals159@gmail.com>
2021-12-24 16:37:46 +0900

    main ~ing

--- app/src/main/java/com/example/heartbeat/StartActivity.java
+++ app/src/main/java/com/example/heartbeat/StartActivity.java
@@ -9,6 +9,6 @@ public class StartActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.check_page);
+        setContentView(R.layout.activity_check);
     }
 }
